2b617ad2c9dc5525820d2e5f150e2164e2c56eea
tonotpanda <jcastillop2324@politecnics.barcelona>
2025-03-14 16:54:30 +0100

    integra boton de agrega eventos

--- app/src/main/java/com/example/culturewaveinter/FragmentHome.kt
+++ app/src/main/java/com/example/culturewaveinter/FragmentHome.kt
@@ -1,8 +1,25 @@
 package com.example.culturewaveinter
 
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 
-class FragmentHome: Fragment(R.layout.fragmenthome) {
+class FragmentHome : Fragment(R.layout.fragmenthome) {
 
+    private lateinit var imageViewNewEvent: ImageView // Declara la variable como ImageView
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Usa view.findViewById para encontrar el ImageView
+        imageViewNewEvent = view.findViewById(R.id.buttonNewEvents)
+
+        // Configura el clic del ImageView
+        imageViewNewEvent.setOnClickListener {
+            val intent = Intent(activity, crearEventosActivity::class.java)
+            startActivity(intent) // Inicia la actividad
+        }
+    }
 }
---
+++ app/src/main/java/com/example/culturewaveinter/crearEventosActivity.kt
@@ -0,0 +1,12 @@
+package com.example.culturewaveinter
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class crearEventosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.crear_eventos_layout)
+    }
+}
